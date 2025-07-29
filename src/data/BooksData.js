666376449ff26f574440e1d2d4b2029860ad0ba9
priyanka1xl <priyanka@1xl.com>
2025-07-29 13:38:02 +0530

    Json data for reusable components

---
+++ src/data/BooksData.js
@@ -0,0 +1,106 @@
+
+
+export const BooksData = [
+    // What is Dreams to Reality All About?
+  {
+    id: "DreamsToReality",
+    title: "What is Dreams to Reality All About?",
+    description: [
+      "Dreams to Reality is the true story of two young siblings and child prodigies who turned bold dreams into nationwide impact-completing 120+ events in 50 days, inspiring over 50,000 lives across India.",
+      "Through five powerful 50-day missions, they mastered discipline, embraced challenges, and proved that with the right mindset, age is never a limitation.",
+      "This book isn’t just their story; it’s a blueprint for anyone ready to take action, break barriers, and lead with purpose.",
+    ],
+    image: "/Images/BookCopy.webp"
+  },
+//   Mission50 – Our Boldest Leap Yet
+  {
+    id: "mission50",
+    title: "Mission50 – Our Boldest Leap Yet",
+    description: [
+      "We wanted to test everything we’d learned. So we set out on Mission50 in 2024, a 50-day tour across India with 120+ free events and a goal to reach 50,000 people.",
+      "It wasn’t a campaign. It was a commitment.",
+      "With our mom on the road and our dad cheering us on from afar, we ran this mission ourselves, planning, preparing, speaking, and sometimes just showing up even when we were exhausted.",
+      "We documented it all on JJMission Diary, a space where we share personal reflections, experiments, life hacks, and everything we learned in real time."
+    ],
+    image: "/Images/mission-50.webp"
+  },
+//   The Road Wasn’t Easy
+   {
+    id: "RoadWasntEasy",
+    title: "The Road Wasn’t Easy",
+    description: [
+      "We faced illnesses. Language barriers. Travel fatigue. Sleepless nights.",
+      "But we learned that challenges aren’t setbacks, they’re reminders that we’re doing something real.",
+      "Every struggle added depth. Every obstacle made the mission matter more.",
+    ],
+    image: "/Images/RoadWasntEasy.webp"
+  },
+//   Team Dreams to Reality – The Real Heroes
+  {
+    id: "RealHeroes",
+    title: "Team Dreams to Reality – The Real Heroes",
+    description: [
+      "We couldn’t have done it alone. This was a team journey.",
+      "Our mom was our co-creator, companion, and motivator on the road.",
+      "Our dad kept us grounded from miles away.",
+      "It takes a village, and we’re proud of ours.",
+    ],
+    image: "/Images/team.webp"
+  },
+//   What We Learned
+  {
+    id: "learned",
+    title: "What We Learned",
+    description: [
+      "Age is irrelevant.",
+      "We started at 10. We weren’t waiting for permission; we gave it to ourselves.",
+      "Giving creates growth.",
+      "Every time we shared our ideas, our time, or our voice, we received something bigger: support, energy, and a deeper meaning.",
+    ],
+    image: "/Images/what-learned.webp"
+  },
+//   The Next Chapter: Dreams to Reality
+  {
+    id: "TheNextChapter",
+    title: "The Next Chapter: Dreams to Reality",
+    description: [
+      "Our latest initiative, Dreams to Reality, is about sharing the blueprint of our journey so others can start their own.",
+      "It's not about being fearless. It’s about showing up even when you’re scared.",
+      "It’s for those who are tired of waiting and ready to take one small step toward their dreams.",
+    ],
+    image: "/Images/TheNextChapter.webp"
+  },
+//   Stories That Stay With Us
+  {
+    id: "storiesStay",
+    title: "The Next Chapter: Dreams to Reality",
+    description: [
+      "Behind every mission, campaign, and breakthrough, there’s a spark, and we’ve captured those sparks across two platforms designed to keep the momentum alive, to keep inspiring, and to turn ideas into impact for thousands more.",
+    ],
+    image: "/Images/stories.webp"
+  },
+//   Dreams to Reality isn’t just a story; it’s an inspiration!
+{
+    id: "Inspiration",
+    title: "Dreams to Reality isn’t just a story; it’s an inspiration!",
+    description: [
+      "And these platforms are how we keep that inspiration alive, through action, creativity, and connection.",
+      "Be part of the next chapter.",
+      "Because the real magic begins when you take the first step.",
+    ],
+    image: "/Images/Inspiration.webp"
+  },
+//   Get Your Copy Now – Limited Stock Available!
+  {
+    id: "LimitedBook",
+    title: "Get Your Copy Now – Limited Stock Available!",
+    description: [
+      "We’ve printed only a few copies of Dreams to Reality for this special giveaway - and they’re going fast.",
+      "Once these copies are gone, it could be months before more are available.",
+      "The good news? Your copy is still available - right now",
+      "But time is running out…",
+      "If you’re serious about turning your goals into reality, don’t wait - grab your copy today before it’s gone!"
+    ],
+    image: "/Images/LimitedBook.webp"
+  },
+];
