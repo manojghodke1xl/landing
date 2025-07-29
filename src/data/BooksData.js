01b8e124dc4bbf0523265451f582edba956f6e0e
priyanka1xl <priyanka@1xl.com>
2025-07-29 16:28:53 +0530

    Json data for Reusable components

--- src/data/BooksData.js
+++ src/data/BooksData.js
@@ -1,106 +1,266 @@
 
 
-export const BooksData = [
-    // What is Dreams to Reality All About?
-  {
-    id: "DreamsToReality",
-    title: "What is Dreams to Reality All About?",
-    description: [
-      "Dreams to Reality is the true story of two young siblings and child prodigies who turned bold dreams into nationwide impact-completing 120+ events in 50 days, inspiring over 50,000 lives across India.",
-      "Through five powerful 50-day missions, they mastered discipline, embraced challenges, and proved that with the right mindset, age is never a limitation.",
-      "This book isn’t just their story; it’s a blueprint for anyone ready to take action, break barriers, and lead with purpose.",
+export const BooksData = {
+    // Dreams to Reality
+    "mission": [
+        {
+            id: "DreamsToReality",
+            title: "What is Dreams to Reality All About?",
+            description: [
+                "Dreams to Reality is the true story of two young siblings and child prodigies who turned bold dreams into nationwide impact-completing 120+ events in 50 days, inspiring over 50,000 lives across India.",
+                "Through five powerful 50-day missions, they mastered discipline, embraced challenges, and proved that with the right mindset, age is never a limitation.",
+                "This book isn’t just their story; it’s a blueprint for anyone ready to take action, break barriers, and lead with purpose.",
+            ],
+            image: "/Images/BookCopy.webp"
+        },
+        //   Mission50 
+        {
+            id: "mission50",
+            title: "Mission50 – Our Boldest Leap Yet",
+            description: [
+                "We wanted to test everything we’d learned. So we set out on Mission50 in 2024, a 50-day tour across India with 120+ free events and a goal to reach 50,000 people.",
+                "It wasn’t a campaign. It was a commitment.",
+                "With our mom on the road and our dad cheering us on from afar, we ran this mission ourselves, planning, preparing, speaking, and sometimes just showing up even when we were exhausted.",
+                "We documented it all on JJMission Diary, a space where we share personal reflections, experiments, life hacks, and everything we learned in real time."
+            ],
+            image: "/Images/mission-50.webp"
+        },
+        //   The Road Wasn’t Easy
+        {
+            id: "RoadWasntEasy",
+            title: "The Road Wasn’t Easy",
+            description: [
+                "We faced illnesses. Language barriers. Travel fatigue. Sleepless nights.",
+                "But we learned that challenges aren’t setbacks, they’re reminders that we’re doing something real.",
+                "Every struggle added depth. Every obstacle made the mission matter more.",
+            ],
+            image: "/Images/RoadWasntEasy.webp"
+        },
+        //   Team Dreams to Reality – The Real Heroes
+        {
+            id: "RealHeroes",
+            title: "Team Dreams to Reality – The Real Heroes",
+            description: [
+                "We couldn’t have done it alone. This was a team journey.",
+                "Our mom was our co-creator, companion, and motivator on the road.",
+                "Our dad kept us grounded from miles away.",
+                "It takes a village, and we’re proud of ours.",
+            ],
+            image: "/Images/team.webp"
+        },
+        //   What We Learned
+        {
+            id: "learned",
+            title: "What We Learned",
+            description: [
+                "Age is irrelevant.",
+                "We started at 10. We weren’t waiting for permission; we gave it to ourselves.",
+                "Giving creates growth.",
+                "Every time we shared our ideas, our time, or our voice, we received something bigger: support, energy, and a deeper meaning.",
+            ],
+            image: "/Images/what-learned.webp"
+        },
+        //  Next Chapter: Dreams to Reality
+        {
+            id: "TheNextChapter",
+            title: "The Next Chapter: Dreams to Reality",
+            description: [
+                "Our latest initiative, Dreams to Reality, is about sharing the blueprint of our journey so others can start their own.",
+                "It's not about being fearless. It’s about showing up even when you’re scared.",
+                "It’s for those who are tired of waiting and ready to take one small step toward their dreams.",
+            ],
+            image: "/Images/TheNextChapter.webp"
+        },
+        //   Stories That Stay With Us
+        {
+            id: "storiesStay",
+            title: "The Next Chapter: Dreams to Reality",
+            description: [
+                "Behind every mission, campaign, and breakthrough, there’s a spark, and we’ve captured those sparks across two platforms designed to keep the momentum alive, to keep inspiring, and to turn ideas into impact for thousands more.",
+            ],
+            image: "/Images/stories.webp"
+        },
+        //   Dreams to Reality - inspiration!
+        {
+            id: "Inspiration",
+            title: "Dreams to Reality isn’t just a story; it’s an inspiration!",
+            description: [
+                "And these platforms are how we keep that inspiration alive, through action, creativity, and connection.",
+                "Be part of the next chapter.",
+                "Because the real magic begins when you take the first step.",
+            ],
+            image: "/Images/Inspiration.webp"
+        },
+        //   Limited Stock 
+        {
+            id: "LimitedBook",
+            title: "Get Your Copy Now – Limited Stock Available!",
+            description: [
+                "We’ve printed only a few copies of Dreams to Reality for this special giveaway - and they’re going fast.",
+                "Once these copies are gone, it could be months before more are available.",
+                "The good news? Your copy is still available - right now",
+                "But time is running out…",
+                "If you’re serious about turning your goals into reality, don’t wait - grab your copy today before it’s gone!"
+            ],
+            image: "/Images/LimitedBook.webp"
+        },
     ],
-    image: "/Images/BookCopy.webp"
-  },
-//   Mission50 – Our Boldest Leap Yet
-  {
-    id: "mission50",
-    title: "Mission50 – Our Boldest Leap Yet",
-    description: [
-      "We wanted to test everything we’d learned. So we set out on Mission50 in 2024, a 50-day tour across India with 120+ free events and a goal to reach 50,000 people.",
-      "It wasn’t a campaign. It was a commitment.",
-      "With our mom on the road and our dad cheering us on from afar, we ran this mission ourselves, planning, preparing, speaking, and sometimes just showing up even when we were exhausted.",
-      "We documented it all on JJMission Diary, a space where we share personal reflections, experiments, life hacks, and everything we learned in real time."
+    //  What Will You Discover section
+    whatYouWillDiscover: {
+        heading: "What Will You Discover in This Book?",
+        items: [
+            {
+                title: "Real Stories of Action, Not Just Advice",
+                description:
+                    "Follow the journey of two young siblings who turned learning into impact, completing five 50-day missions that inspired 50,000+ lives across India"
+            },
+            {
+                title: "A Blueprint for Purpose-Driven Growth",
+                description:
+                    "Learn how they developed skills like discipline, time management, public speaking, and focus through hands-on experience, not just theory."
+            },
+            {
+                title: "How to Start Before You Feel Ready",
+                description:
+                    "See how Jainam and Jivika launched projects like JJFuntime, 1XL, Jainam Jivika Foundation, and more by starting small and showing up consistently."
+            },
+            {
+                title: "Lessons That Go Beyond Age or Background",
+                description:
+                    "Discover how their faith, family, and mindset helped them tackle challenges from public speaking fears to health setbacks and come out stronger every time."
+            },
+            {
+                title: "Tools to Turn Dreams Into Daily Practice",
+                description:
+                    "From reading 50 books in 50 days to mastering 50 skills, this book shares the exact missions and frameworks that helped them turn learning into leadership."
+            },
+            {
+                title: "A Deeper Meaning of Success",
+                description:
+                    "Explore how giving back, serving others, and creating impact became more powerful than any certificate or award."
+            },
+
+        ],
+        // Every Year, Our Growth in section
+        growthInMotion: {
+            heading: "Every Year, A New Leap – Our Growth in Motion",
+            years: [
+                {
+                    year: "2019",
+                    description:
+                        "Launched JJFuntime, a YouTube channel at ages 7 and 4, igniting curiosity through fun, science-led experiments for children around the world."
+                },
+                {
+                    year: "2020",
+                    description:
+                        "Learned a full religious book in just 50 days, in Prakrit, to challenge our minds and connect with our roots, showing that age doesn’t define discipline."
+                },
+                {
+                    year: "2021",
+                    description:
+                        "Attended 50 networking events in 50 days. We listened, learned, and asked questions, gaining clarity and confidence from some of India’s most inspiring voices."
+                },
+                {
+                    year: "2022",
+                    description:
+                        "Completed 50 books in 50 days. From leadership to mindset, every page taught us that knowledge becomes power when paired with action."
+                },
+                {
+                    year: "2023",
+                    description:
+                        "Learned 50 new skills in 50 days, from communication and growth to discipline and time management. It was a year of application, not just accumulation."
+                },
+                {
+                    year: "2024",
+                    description:
+                        "Our boldest leap - Mission50. We hosted 120+ events in 50 days across India, reaching over 50,000 people through free, high-energy sessions in schools, NGOs, and colleges."
+                },
+            ],
+            finalYear: {
+                year: "2025",
+                title: "A New Chapter Begins.",
+                description:
+                    "We’re taking everything we’ve learned and scaling it into something bigger - tools, blueprints, and guided missions to help others ignite their own journeys. What’s coming next? Let’s just say… It’s only the beginning."
+            },
+        },
+    },
+    // Dreams to Reality impact
+    "impactStory": [
+        {
+            id: "impactSection",
+            title: "Dreams to Reality is a testament to how purpose, not age, creates impact.",
+            intro: "This is the true story of two young siblings and child prodigies who transformed their learnings into a mission of giving back reaching over 50,000 people across India through 120+ events in just 50 days. Their journey wasn’t just about personal growth; it was rooted in service, contribution, and the desire to spark possibility in every life they touched.",
+            listTitle: "Through their 50-day mission, they:",
+            points: [
+                "Turned vision into impact - one day, one event at a time.",
+                "Faced doubts, setbacks, and uncertainty with resilience.",
+                "Inspired 50,000+ people by showing what’s possible with clarity and courage.",
+                "Focused on growth, learning, and service, rather than just goals."
+            ],
+
+            outro: {
+                text: "This book reminds us that you don’t have to be extraordinary to create change; you just have to be intentional.",
+                heading: "It’s not a story of luck; it’s a journey of choice, discipline, and heart."
+            }
+        }
     ],
-    image: "/Images/mission-50.webp"
-  },
-//   The Road Wasn’t Easy
-   {
-    id: "RoadWasntEasy",
-    title: "The Road Wasn’t Easy",
-    description: [
-      "We faced illnesses. Language barriers. Travel fatigue. Sleepless nights.",
-      "But we learned that challenges aren’t setbacks, they’re reminders that we’re doing something real.",
-      "Every struggle added depth. Every obstacle made the mission matter more.",
+    // Breaking All the Rules section
+    "BreakingRulesSection": [
+        {
+            id: "breakingRules",
+            title: "We Turned Dreams into Reality by Breaking All the Rules!",
+            subtitle:
+                "We never had a big network or formal training when we started. Even better, we didn’t rely on what most people consider essential for success. Our journey had:",
+            intro:
+                "We didn’t have shortcuts, fame, or a head start.",
+            points: [
+                "What we did have was stubborn determination,",
+                "A strong willingness to learn,",
+                "Consistency through every challenge,",
+                "And a habit of taking on one bold mission every year."
+            ],
+            image: "/Images/breaking-all-rules.webp",
+            outro: {
+                text: "Sounds impossible, right? But here’s the truth:",
+                heading: "Success comes from actions, not circumstances."
+            }
+        }
     ],
-    image: "/Images/RoadWasntEasy.webp"
-  },
-//   Team Dreams to Reality – The Real Heroes
-  {
-    id: "RealHeroes",
-    title: "Team Dreams to Reality – The Real Heroes",
-    description: [
-      "We couldn’t have done it alone. This was a team journey.",
-      "Our mom was our co-creator, companion, and motivator on the road.",
-      "Our dad kept us grounded from miles away.",
-      "It takes a village, and we’re proud of ours.",
+    // Our Approach to Growth section
+    "approachSections": [
+        {
+            id: "approach",
+            heading: "Our Approach to Growth Has Given Us More Freedom Than We Ever Imagined!",
+            image: "/Images/freedom.webp",
+            intro: " By following a structured system of 50-day challenges, we were able to focus on what truly matters:",
+            points: [
+                "Taking on new challenges without hesitation.",
+                "Building confidence through real experiences.",
+                "Learning, growing, and inspiring others along the way.",
+                "Creating a life of impact by helping others achieve their dreams."
+            ],
+            outro: "Through this approach, we’ve realised that success isn’t just about achievements - it’s about freedom, growth, and making a difference.",
+        },
     ],
-    image: "/Images/team.webp"
-  },
-//   What We Learned
-  {
-    id: "learned",
-    title: "What We Learned",
-    description: [
-      "Age is irrelevant.",
-      "We started at 10. We weren’t waiting for permission; we gave it to ourselves.",
-      "Giving creates growth.",
-      "Every time we shared our ideas, our time, or our voice, we received something bigger: support, energy, and a deeper meaning.",
-    ],
-    image: "/Images/what-learned.webp"
-  },
-//   The Next Chapter: Dreams to Reality
-  {
-    id: "TheNextChapter",
-    title: "The Next Chapter: Dreams to Reality",
-    description: [
-      "Our latest initiative, Dreams to Reality, is about sharing the blueprint of our journey so others can start their own.",
-      "It's not about being fearless. It’s about showing up even when you’re scared.",
-      "It’s for those who are tired of waiting and ready to take one small step toward their dreams.",
-    ],
-    image: "/Images/TheNextChapter.webp"
-  },
-//   Stories That Stay With Us
-  {
-    id: "storiesStay",
-    title: "The Next Chapter: Dreams to Reality",
-    description: [
-      "Behind every mission, campaign, and breakthrough, there’s a spark, and we’ve captured those sparks across two platforms designed to keep the momentum alive, to keep inspiring, and to turn ideas into impact for thousands more.",
-    ],
-    image: "/Images/stories.webp"
-  },
-//   Dreams to Reality isn’t just a story; it’s an inspiration!
-{
-    id: "Inspiration",
-    title: "Dreams to Reality isn’t just a story; it’s an inspiration!",
-    description: [
-      "And these platforms are how we keep that inspiration alive, through action, creativity, and connection.",
-      "Be part of the next chapter.",
-      "Because the real magic begins when you take the first step.",
-    ],
-    image: "/Images/Inspiration.webp"
-  },
-//   Get Your Copy Now – Limited Stock Available!
-  {
-    id: "LimitedBook",
-    title: "Get Your Copy Now – Limited Stock Available!",
-    description: [
-      "We’ve printed only a few copies of Dreams to Reality for this special giveaway - and they’re going fast.",
-      "Once these copies are gone, it could be months before more are available.",
-      "The good news? Your copy is still available - right now",
-      "But time is running out…",
-      "If you’re serious about turning your goals into reality, don’t wait - grab your copy today before it’s gone!"
-    ],
-    image: "/Images/LimitedBook.webp"
-  },
-];
+    // Dreams to Reality - Another Ordinary Book
+    "OrdinaryBookSection": [
+        {
+            id: "OrdinaryBook",
+            title: "Dreams to Reality is NOT Just Another Ordinary Book!",
+            intro: "Unlike typical motivational books, Dreams to Reality isn’t filled with complex theories or impractical advice.",
+            subTitle: "Instead, this book offers:",
+            points: [
+                "Actionable guidance to help you move forward.",
+                "Real-life experiences from two young achievers who made a tangible impact.",
+                "Lessons learned through personal challenges and growth."
+            ],
+            image: "/Images/not-a-ordinary.webp",
+            outro: {
+                title: "The best part? You don’t have to figure it all out on your own.",
+                description:
+                    "Dreams to Reality will show you exactly how to break past doubts, take bold action, and start achieving what once felt impossible."
+            },
+        },
+    ]
+}
+
